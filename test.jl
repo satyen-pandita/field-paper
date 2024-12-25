41c880f2a5269e1db23c81ad025f0c4188fbc43b
shreyak4 <shreyak4@illinois.edu>
2024-12-24 17:59:04 -0800

    Refactor initialization weights and enhance test coverage for utility calculations

--- test.jl
+++ test.jl
@@ -1,18 +1,44 @@
 include("moments.jl");
 
+prim, smm_params = Initialize(Min_x_f_A,  true, true)
 
-for sc in [true, false], home in [true, false]
-    x,y = res[(sc, home)]
-    prim, smm_params = Initialize(x, sc, home)
-    @show sc, home
-    println(gen_means(prim, smm_params, sc))
+gen_means(prim, smm_params, true)
+
+
+prim, smm_params = Initialize(Min_x_f_eta,  true, false);
+gen_means(prim, smm_params, true)
+
+
+
+
+# Results with modified utility 
+
+# res = Dict(false => ([0.122717, 71.3624], 4.08059), true  => ([0.0201509, 19.4524, 5.81422, 0.0453233], 3.6617))
+
+# Results without modified utility
+# res = Dict(true => ([0.00230059, 384.294, 17.5517, 0.0233759], 3.77918), false  => ([0.0137712, 2163.6], 4.13795))
+
+
+
+
+# Let's take average wm, wf. 
+Am = 1.0
+Af = collect(range(0.01, 10.0, length=100))
+Us = zeros(length(Af))
+wm = 125.0
+wf = 102.0
+hh_type = HH_Type(wm, wf, 0.0, 0.0)
+for (j, af) in enumerate(Af) 
+    prim, smm_params = Initialize([Am, af], false, true)
+    actions = action_set(prim, smm_params)
+    probs = prob_actions_hh(prim, smm_params, hh_type)
+    # What is the average utility  
+    u_avg = sum([probs[i]*u(prim, smm_params, hh_type, c(prim, wm*actions[i][1] + wf*actions[i][2], H(prim, smm_params, actions[i][5], actions[i][6], wm*actions[i][1] + wf*actions[i][2])), 
+                actions[i][3], actions[i][4], actions[i][6], H(prim, smm_params, actions[i][5], actions[i][6], wm*actions[i][1] + wf*actions[i][2])) for i in eachindex(actions)]) 
+    Us[j] = u_avg
 end
+# Derivative of Us w.r.t Af
+Dus = diff(Us)./diff(Af) 
 
-x,y = res[(true, true)]
-prim, smm_params = Initialize(x, true, true)
-gen_means(prim, smm_params,true)
-
-
-x,y = res[(false, true)]
-prim, smm_params = Initialize(x, false, true)
-gen_means(prim, smm_params,false)
+using Plots
+# savefig("utility.png")
