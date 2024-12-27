d7731a8ed36246408d15a9a2ac7deadad61158b7
shreyak4 <shreyak4@illinois.edu>
2024-12-27 16:27:03 -0500

    added write-up results

--- earlier_results.jl
+++ earlier_results.jl
@@ -7,6 +7,8 @@
 # (false, true) => ([0.0839646, 537.476], 4.66374)
 
 # Without modified utility(Used in the write-up)
+# res = Dict(false => ([0.122717, 71.3624], 4.08059), true  => ([0.0201509, 19.4524, 5.81422, 0.0453233], 3.6617))
+
 
 # Results with modified utility (but no stigma in the South) - will write about this.
 # res = Dict(false => ([0.122717, 71.3624], 4.08059), true  => ([0.0201509, 19.4524, 5.81422, 0.0453233], 3.6617))
