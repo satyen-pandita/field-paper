b56be6441d771844aadd49f259ebce71f8e4dea5
shreyak4 <shreyak4@illinois.edu>
2024-12-27 16:30:26 -0500

    added earlier results for analysis

--- earlier_results.jl
+++ earlier_results.jl
@@ -1,13 +1,19 @@
 
 
 
-
 # Without modified utility (newest)
 # (true, true)  => ([0.00498952, 172.283, 28.2292, 0.0714194], 4.09295)
 # (false, true) => ([0.0839646, 537.476], 4.66374)
 
-# Results with modified utility 
+# Without modified utility(Used in the write-up)
 # res = Dict(false => ([0.122717, 71.3624], 4.08059), true  => ([0.0201509, 19.4524, 5.81422, 0.0453233], 3.6617))
 
-# Results without modified utility
-# res = Dict(true => ([0.00230059, 384.294, 17.5517, 0.0233759], 3.77918), false  => ([0.0137712, 2163.6], 4.13795))
+
+# Results with modified utility (but no stigma in the South) - will write about this.
+# res = Dict(false => ([0.122717, 71.3624], 4.08059), true  => ([0.0201509, 19.4524, 5.81422, 0.0453233], 3.6617))
+
+
+# Results with modified utility (but with stigma in the South too) - will write about this.
+
+# false => ([0.0724101, 36.9881], 4.04397)
+# true  => ([0.132797, 136.492, 27.5235, 0.0306309], 3.76701)
